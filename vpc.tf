9c2df44c0523108f9a8bdd55a51ab6c224c69192
ahmed12shire <ahmed12shire@gmail.com>
2024-06-08 19:15:28 +0530

    public subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "lms-projectb-vpc" {
     Name = "lms-projectb-vpc"
   }
 }
+
+# PUBLICE SUBNET
+resource "aws_subnet" "lms-projectb-pub-subnet" {
+  vpc_id     = aws_vpc.lms-projectb-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone_id = "ca-central-1a"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "lms-projectb-pub-subnet"
+  }
+}
